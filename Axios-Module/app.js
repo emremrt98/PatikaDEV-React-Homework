b560a9a78aac26d76a378de3d4626201b84c4c47
Emre Mert <emre.mrt98@gmail.com>
2022-07-12 01:58:49 +0300

    Emre MERT

---
+++ Axios-Module/app.js
@@ -0,0 +1,6 @@
+import GetData from "./getData.js"
+
+const result = GetData(2);
+
+
+const data = result.then(res => console.log(res));
---
+++ Axios-Module/getData.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+
+async function getData(userID) {
+    let baseURL = "https://jsonplaceholder.typicode.com/users/" + userID
+    const data = await axios(baseURL).then(res => res.data);
+    //console.log(data);
+    baseURL = "https://jsonplaceholder.typicode.com/posts?userId=" + userID;
+    const dataPost = await axios(baseURL,{
+        params: {
+          _limit: 1
+         }
+      }).then(res => res.data);
+    //console.log(baseURL);
+ 
+    return {
+        data,
+        dataPost
+    }
+}
+
+export default getData;
+
+
+
+
